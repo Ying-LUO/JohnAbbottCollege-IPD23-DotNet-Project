c11f004d714e7c27b15ebee02ec91a146b9933af
Ying LUO <ying.luo@johnabbottcollege.net>
2020-12-18 10:59:06 -0500

    Update database script and diagram

--- SimpleJiraDb_Initial.sql
+++ SimpleJiraDb_Initial.sql
@@ -1,7 +1,9 @@
 USE SimpleJiraDB;
 GO
 
+DROP TABLE Projects;
 DROP TABLE Sprints;
+DROP TABLE UserStories;
 DROP TABLE Issues;
 DROP TABLE Users;
 DROP TABLE Teams;
@@ -35,15 +37,26 @@ ADD
 ;
 go
 
-/***** Table No. 3 - Sprints ****/
+/***** Table No. 3 - Projects ****/
+CREATE TABLE Projects
+(
+	ProjectId int IDENTITY(1,1) NOT NULL, -- auto-generated number
+	Name nvarchar(20) NOT NULL,
+	CONSTRAINT PK_Projects PRIMARY KEY CLUSTERED (ProjectId ASC)
+)
+;
+go
+
+/***** Table No. 4 - Sprints ****/
 CREATE TABLE Sprints
 (
 	SprintId int IDENTITY(1,1) NOT NULL, -- auto-generated number
 	Name nvarchar(20) NOT NULL,
-	Description nvarchar(200) NOT NULL,
+	Description nvarchar(255) NOT NULL,
 	StartDate DATETIME NOT NULL,
-	CompleteDate DATETIME NULL,
+	ReleaseDate DATETIME NULL,
 	Status nvarchar(20) NOT NULL,
+	ProjectId int NOT NULL,
 	OwnerId int NOT NULL,
 	CONSTRAINT PK_Sprints PRIMARY KEY CLUSTERED (SprintId ASC)
 )
@@ -52,12 +65,39 @@ go
 
 ALTER TABLE Sprints
 ADD
-	CONSTRAINT CK_Sprints_Status CHECK (Status IN ('Planning', 'Ongoing', 'Completed')),
-	CONSTRAINT FK_Sprints_Teams FOREIGN KEY (OwnerId) REFERENCES Teams(TeamId)
+	CONSTRAINT CK_Sprints_Status CHECK (Status IN ('Planning', 'Ongoing', 'Released')),
+	CONSTRAINT FK_Sprints_Teams FOREIGN KEY (OwnerId) REFERENCES Teams(TeamId),
+	CONSTRAINT FK_Sprints_Projects FOREIGN KEY (ProjectId) REFERENCES Projects(ProjectId)
 ;
 go
 
-/***** Table No. 4 - Issues ****/
+
+/***** Table No. 5 - UserStories ****/
+CREATE TABLE UserStories
+(
+	UserStoryId int IDENTITY(1,1) NOT NULL, -- auto-generated number
+	Name nvarchar(20) NOT NULL,
+	Description nvarchar(255) NOT NULL,
+	StartDate DATETIME NOT NULL,
+	CompleteDate DATETIME NULL,
+	Point int NOT NULL,
+	Photo VARBINARY(MAX) NULL,
+	Status nvarchar(20) NOT NULL,
+	OwnerId int NOT NULL,
+	CONSTRAINT PK_UserStories PRIMARY KEY CLUSTERED (UserStoryId ASC)
+)
+;
+go
+
+ALTER TABLE UserStories
+ADD
+	CONSTRAINT CK_UserStories_Point CHECK (Point<100 AND Point>0),
+	CONSTRAINT CK_UserStories_Status CHECK (Status IN ('Todo', 'Documenting', 'Pending for Validation', 'Ready', 'DEV','TEST','DONE')),
+	CONSTRAINT FK_UserStories_Users FOREIGN KEY (OwnerId) REFERENCES Users(UserId)
+;
+go
+
+/***** Table No. 6 - Issues ****/
 CREATE TABLE Issues
 (
 	IssueId int IDENTITY(1,1) NOT NULL, -- auto-generated number
@@ -65,10 +105,12 @@ CREATE TABLE Issues
 	Description nvarchar(200) NOT NULL,
 	StartDate DATETIME NOT NULL,
 	CompleteDate DATETIME NULL,
+	Priority int NOT NULL,
 	Photo VARBINARY(MAX) NULL,
 	Category nvarchar(20) NOT NULL,
 	Status nvarchar(20) NOT NULL,
 	OwnerId int NOT NULL,
+	UserStoryId int NOT NULL,
 	CONSTRAINT PK_Issues PRIMARY KEY CLUSTERED (IssueId ASC)
 )
 ;
@@ -76,9 +118,11 @@ go
 
 ALTER TABLE Issues
 ADD
+	CONSTRAINT CK_Issues_Priority CHECK (Priority<6 AND Priority >0),
 	CONSTRAINT CK_Issues_Category CHECK (Status IN ('UserStory', 'Defect', 'Task', 'Backlog')),
-	CONSTRAINT CK_Issues_Status CHECK (Status IN ('Todo', 'InProcess', 'UnderReview', 'Done')),
-	CONSTRAINT FK_Issues_Users FOREIGN KEY (OwnerId) REFERENCES Users(UserId)
+	CONSTRAINT CK_Issues_Status CHECK (Status IN ('Todo', 'InProcess', 'Blocked', 'UnderReview', 'UnderVerification', 'Verified', 'Resolved')),
+	CONSTRAINT FK_Issues_Users FOREIGN KEY (OwnerId) REFERENCES Users(UserId),
+	CONSTRAINT FK_Issues_UserStoryId FOREIGN KEY (UserStoryId) REFERENCES UserStories(UserStoryId)
 ;
 go
 
